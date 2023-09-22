ea3bcd30b4607157c2f3643a8e0d6f2d7b2fedc4
Julius-3367 <korirjulius001@gmail.com>
2023-09-22 00:24:58 -0700

    updates

--- _atoi.c
+++ _atoi.c
@@ -1,44 +1,74 @@
 #include "shell.h"
 
 /**
- * main - entry point
- * @ac: arg count
- * @av: arg vector
+ * interactive - returns true if shell is interactive mode
+ * @info: struct address
  *
- * Return: 0 on success, 1 on error
+ * Return: 1 if interactive mode, 0 otherwise
  */
-int main(int ac, char **av)
+int interactive(info_t *info)
 {
-	info_t info[] = { INFO_INIT };
-	int fd = 2;
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
 
-	asm ("mov %1, %0\n\t"
-		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
+/**
+ * is_delim - checks if character is a delimeter
+ * @c: the char to check
+ * @delim: the delimeter string
+ * Return: 1 if true, 0 if false
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
 
-	if (ac == 2)
+/**
+ *_isalpha - checks for alphabetic character
+ *@c: The character to input
+ *Return: 1 if c is alphabetic, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ *_atoi - converts a string to an integer
+ *@s: the string to be converted
+ *Return: 0 if no numbers in string, converted number otherwise
+ */
+
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
 	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				_eputs(av[0]);
-				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
-				exit(127);
-			}
-			return (EXIT_FAILURE);
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
 		}
-		info->readfd = fd;
+		else if (flag == 1)
+			flag = 2;
 	}
-	populate_env_list(info);
-	read_history(info);
-	hsh(info, av);
-	return (EXIT_SUCCESS);
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
 }
